674a3c5b8bb08d9afa667568e6c2590b920faddf
Lenny Amstutz <lennyamstutz@gmail.com>
2026-01-12 12:48:10 +0100

    fix test

--- sets.js
+++ sets.js
@@ -1,19 +1,8 @@
 export function unique(elements) {
-  const result = [];
-  for (const e of elements) {
-    if (!result.includes(e)) {
-      result.push(e);
-    }
-  }
   return [...new Set(elements)];
 }
 
 export function diff(left, right) {
   const rightSet = new Set(right);
-  for (const l of left) {
-    if (!right.includes(l)) {
-      result.push(l);
-    }
-  }
   return left.filter(e => !rightSet.has(e));
 }
